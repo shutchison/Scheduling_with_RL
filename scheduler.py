dc76dc5f7ad112c5275bb24bcf6ae4f424e446eb
Joel Bland <jbland@ksu.edu>
2023-02-22 13:07:50 -0600

    Merged conflicting commits

--- scheduler.py
+++ scheduler.py
@@ -177,25 +177,31 @@ class Scheduler():
         for m in self.machines:
             if (m.avail_mem >= job.req_mem) and (m.avail_cpus >= job.req_cpus) and (m.avail_gpus >= job.req_gpus):
                 
+                # not all nodes have both GPUs and CPUs, so init each margin to 0
                 mem_margin = 0.0
                 cpu_margin = 0.0
                 gpu_margin = 0.0
 
-                n_attr = 0
+                # count how many attributes the node has to normalize the final margin
+                n_attributes = 0
 
                 if m.total_mem > 0:
                     mem_margin = (m.avail_mem - job.req_mem)/m.total_mem
-                    n_attr += 1
+                    n_attributes += 1
 
                 if m.total_cpus > 0:
                     cpu_margin = (m.avail_cpus - job.req_cpus)/m.total_cpus
-                    n_attr += 1
+                    n_attributes += 1
 
                 if m.total_gpus > 0:
                     gpu_margin = (m.avail_gpus - job.req_gpus)/m.total_gpus
-                    n_attr += 1
+                    n_attributes += 1
 
-                fill_margin = (mem_margin + cpu_margin + gpu_margin)/n_attr
+                if n_attributes == 0:
+                    print("{} has no virtual resources (all set to 0).".format(m.node_name))
+                    fill_margin = 10
+                else:
+                    fill_margin = (mem_margin + cpu_margin + gpu_margin)/n_attributes
 
                 if fill_margin <= min_fill_margin:
                     min_fill_margin = fill_margin
