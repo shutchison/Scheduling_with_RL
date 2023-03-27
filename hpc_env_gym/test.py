1de02a6a5802de584521a28879149284c6371b63
shutchison <scott.allen.hutchison@gmail.com>
2023-03-27 12:43:14 -0500

    updated test.py

--- hpc_env_gym/test.py
+++ hpc_env_gym/test.py
@@ -1,8 +1,14 @@
 import hpc_env_gym.envs.hpc_env 
 import gym
 
-env = gym.make("HPCEnv-v0")
-env.reset()
+
+env = gym.make("HPCEnv-v0", render_mode="human")
+
+options = {"machines_csv" : "machines.csv",
+           "jobs_csv" : "jobs.csv"}
+
+env.reset(options=options)
+
 #for i in range(200):
 #    env.step()
     
