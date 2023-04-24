dfa1cc2c680c336ff17763af5b0dca16d10d69e3
Joel Bland <jbland@ksu.edu>
2023-04-24 14:40:56 -0500

    Adapated test.py to collect performance data for our various scheduling methods

--- hpc_env_gym/test.py
+++ hpc_env_gym/test.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import random
 from job import Job
 import torch
+import time
 
 # ===========================================
 #             Init the environment
@@ -47,61 +48,91 @@ decision_name = ""
 
 DECISION_MODE = PPO
 
-for i in range(10000):
-    #print("Step #{}".format(i))
-    #print("observation is: ")
-    #pprint(observation)
-    #print("="*60)
+decision_list = [PPO]
 
-    if DECISION_MODE == RANDOM:
-        node_to_sched = random.randint(0,7)
-        decision_name = "Random"
-    elif DECISION_MODE == BBF:
-        node_to_sched, node = env.scheduler.get_best_bin_first_machine(dummy_job)
-        decision_name = "Best Bin First"
-    elif DECISION_MODE == ORACLE:
-        env.scheduler.use_oracle = True
-        node_to_sched, node = env.scheduler.get_first_available_machine(dummy_job)
-        decision_name = "Oracle"
-    elif DECISION_MODE == SJN:
-        node_to_sched, node = env.scheduler.get_first_available_machine(dummy_job)
-        decision_name = "Shortest Job Next"
-    elif DECISION_MODE == PPO:
-        node_to_sched = agent(torch.tensor(observation)).argmax().item()
-        decision_name = "PPO Agent"
-    else:
-        print("Unknown decision mode")
-        break
-    
-    rankings = env.scheduler.get_best_bin_first_machine_ranking(dummy_job)
+num_trials = 5
 
-    print(f"Future jobs: {len(env.scheduler.future_jobs.queue)}")
-    print(f"Queued jobs: {len(env.scheduler.job_queue)}")
-    #print(dummy_job)
-    #print(env.scheduler.cluster.machines[node_to_sched])
-    print(f"{decision_name} decision is: {node_to_sched}")
-    
-    observation, reward, terminated, truncated, info = env.step(node_to_sched)
+with open("trial_results.csv", "w") as csvfile:
 
-    # Toggle visualization by commenting this out
-    #env.render()
-   
-    dummy_job = Job("job",
-                    observation[0]*1000000,
-                    observation[1],
-                    observation[2],
-                    observation[3]*3600,
-                    0,
-                    0)
+    csvfile.write("Machines File: {}\n".format(env.scheduler.machines_file))
+    csvfile.write("Jobs File: {}\n\n".format(env.scheduler.jobs_file))
+    csvfile.write("Trial,Decision_Method,AQT_sec,Simulation_Time_sec\n")
 
-    if terminated or truncated:
-        print(f"Terminated is {terminated}")
-        print(f"truncated is {truncated}")
-        break
+    for n in range(num_trials):
+        for decision in decision_list:
+            DECISION_MODE = decision
+            start_time=time.time()
 
-average_queue_time = env.scheduler.get_avg_job_queue_time()
+            for i in range(10000):
+                #print("Step #{}".format(i))
+                #print("observation is: ")
+                #pprint(observation)
+                #print("="*60)
 
-print("Machines File: {}".format(env.scheduler.machines_file))
-print("Jobs File: {}".format(env.scheduler.jobs_file))
-print("Decision method: {}".format(decision_name))
-print("Average Queue Time: {} hours".format(round(average_queue_time/3600,2)))
+                if DECISION_MODE == RANDOM:
+                    node_to_sched = random.randint(0,7)
+                    decision_name = "Random"
+                elif DECISION_MODE == BBF:
+                    node_to_sched, node = env.scheduler.get_best_bin_first_machine(dummy_job)
+                    decision_name = "Best Bin First"
+                elif DECISION_MODE == ORACLE:
+                    env.scheduler.use_oracle = True
+                    node_to_sched, node = env.scheduler.get_best_bin_first_machine(dummy_job)
+                    decision_name = "Oracle"
+                elif DECISION_MODE == SJN:
+                    node_to_sched, node = env.scheduler.get_first_available_machine(dummy_job)
+                    decision_name = "Shortest Job Next"
+                elif DECISION_MODE == PPO:
+                    node_to_sched = agent(torch.tensor(observation)).argmax().item()
+                    decision_name = "PPO Agent"
+                else:
+                    print("Unknown decision mode")
+                    break
+                
+                #rankings = env.scheduler.get_best_bin_first_machine_ranking(dummy_job)
+
+                print(f"Future jobs: {len(env.scheduler.future_jobs.queue)}")
+                print(f"Queued jobs: {len(env.scheduler.job_queue)}")
+                #print(dummy_job)
+                #print(env.scheduler.cluster.machines[node_to_sched])
+                print(f"{decision_name} decision is: {node_to_sched}")
+                
+                observation, reward, terminated, truncated, info = env.step(node_to_sched)
+
+                # Toggle visualization by commenting this out
+                #env.render()
+            
+                dummy_job = Job("job",
+                                observation[0]*1000000,
+                                observation[1],
+                                observation[2],
+                                observation[3]*3600,
+                                0,
+                                0)
+
+                if terminated or truncated:
+                    print(f"Terminated is {terminated}")
+                    print(f"truncated is {truncated}")
+                    break
+
+            average_queue_time = env.scheduler.get_avg_job_queue_time()
+
+            duration=round(time.time()-start_time,2)
+
+            print("Machines File: {}".format(env.scheduler.machines_file))
+            print("Jobs File: {}".format(env.scheduler.jobs_file))
+            print("Decision method: {}".format(decision_name))
+            print("Average Queue Time: {} hours".format(round(average_queue_time/3600,2)))
+            print("Simulation took {} seconds".format(duration))
+
+            csvfile.write(str(n+1) + "," + decision_name + "," + str(average_queue_time) + "," + str(duration) + "\n")
+            env.scheduler.reset(env.scheduler.machines_file, env.scheduler.jobs_file)
+            observation = env.scheduler._get_obs()
+            env.scheduler.use_oracle = False
+            dummy_job = Job("job",
+                env.scheduler.job_queue[0].req_mem,
+                env.scheduler.job_queue[0].req_cpus,
+                env.scheduler.job_queue[0].req_gpus,
+                env.scheduler.job_queue[0].req_duration,
+                0,
+                0)
